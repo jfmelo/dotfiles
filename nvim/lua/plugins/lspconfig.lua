3f6124c46362678bd5743acbca7ce8aba636e104
dudemelo <1172271+dudemelo@users.noreply.github.com>
2026-05-17 22:37:08 +0200

    Add godot lsp configuration

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -32,6 +32,19 @@ return {
 
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
+		---
+		---@brief
+		--- https://github.com/godotengine/godot
+		--- Language server for GDScript, used by Godot Engine.
+		local port = os.getenv("GDScript_Port") or "6005"
+		local cmd = vim.lsp.rpc.connect("127.0.0.1", tonumber(port))
+		vim.lsp.config("gdscript", {
+			cmd = cmd,
+			filetypes = { "gd", "gdscript", "gdscript3" },
+			root_markers = { "project.godot", ".git" },
+		})
+		vim.lsp.enable("gdscript")
+
 		local servers = {
 			clangd = {},
 			-- clang_format = {},
